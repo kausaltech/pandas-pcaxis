8e774721e1a52e290eb2e635b234044333f2137f
Juha Yrjölä <juha.yrjola@iki.fi>
2020-06-15 11:19:06 +0300

    Implement support for PXWeb API

---
+++ pandas_pcaxis/pxweb_api.py
@@ -0,0 +1,43 @@
+import requests
+from .px_reader import PxParser
+
+
+class PXWebAPI:
+    def __init__(self, base_url, language='en'):
+        self.base_url = base_url
+        self.language = language
+
+    def get(self, path):
+        url = '%s/api/v1/%s/%s' % (self.base_url, self.language, path)
+        resp = requests.get(url)
+        resp.raise_for_status()
+        return resp.json()
+
+    def post(self, path, params):
+        url = '%s/api/v1/%s/%s' % (self.base_url, self.language, path)
+        resp = requests.post(url, json=params)
+        resp.raise_for_status()
+        return resp
+
+    def list_databases(self):
+        return self.get('')
+
+    def list_topics(self, dbid):
+        return self.get(dbid)
+
+    def get_table(self, path, as_df=False):
+        url = '%s/Resources/PX/Databases/%s' % (self.base_url, path)
+        resp = requests.get(url)
+        resp.raise_for_status()
+        parser = PxParser()
+        pxfile = parser.parse(resp.content)
+        return pxfile
+
+
+if __name__ == '__main__':
+    # api = PXWebAPI('http://api.aluesarjat.fi', 'fi')
+    # p = 'Ympäristötilastot/12_Ymparistotalous/1_Taloudelliset%20tunnusluvut/T1_talousluvut.px'
+
+    api = PXWebAPI('http://pxnet2.stat.fi/PXWeb', 'fi')
+    p = 'StatFin/asu/asas/statfin_asas_pxt_116b.px'
+    print(api.get_table(p))
